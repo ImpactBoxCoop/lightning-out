01c71a0f3ceb3acc9011236fb462326ffdc319ee
AntImpactBox <ant@impactbox.coop>
2025-01-23 07:26:30 +0000

    Flow

--- force-app/main/default/lwc/lightningOutLwc/lightningOutLwc.js
+++ force-app/main/default/lwc/lightningOutLwc/lightningOutLwc.js
@@ -5,7 +5,15 @@
  * @last modified by  : Ant@ImpactBox
  **/
 import { LightningElement, track } from "lwc";
+import getAccountName from "@salesforce/apex/LightningOut.getAccountName";
 
 export default class LightningOutLwc extends LightningElement {
   @track title = "SUCCESS!!!";
+  @track accountName;
+
+  connectedCallback() {
+    getAccountName({}).then((result) => {
+      this.accountName = result;
+    });
+  }
 }
